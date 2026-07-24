ef860614cb7b7acb1a1cd0e30f6aa3fa39f4e448
jankiborad0811-alt <jankiborad0811@gmail.com>
2026-07-24 14:31:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,131 @@
+// Typing Animation
+
+const text = [
+    "BCA Student",
+    "Web Developer",
+    "Backend Developer",
+    "Frontend Developer"
+];
+
+let index = 0;
+let charIndex = 0;
+
+const typing = document.getElementById("typing");
+
+function typeText() {
+
+    if (charIndex < text[index].length) {
+
+        typing.textContent += text[index].charAt(charIndex);
+
+        charIndex++;
+
+        setTimeout(typeText,100);
+
+    }
+    else{
+
+        setTimeout(eraseText,1500);
+
+    }
+
+}
+
+function eraseText(){
+
+    if(charIndex>0){
+
+        typing.textContent=text[index].substring(0,charIndex-1);
+
+        charIndex--;
+
+        setTimeout(eraseText,50);
+
+    }
+
+    else{
+
+        index++;
+
+        if(index>=text.length){
+
+            index=0;
+
+        }
+
+        setTimeout(typeText,300);
+
+    }
+
+}
+
+typeText();
+
+
+
+// Scroll Animation
+
+const hiddenElements=document.querySelectorAll(".hidden");
+
+const observer=new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+});
+
+hiddenElements.forEach(el=>observer.observe(el));
+
+
+
+// Contact Form
+
+function showMessage(){
+
+    alert("Thank you! Your message has been received.");
+
+}
+
+
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 180;
+        const sectionHeight = section.offsetHeight;
+
+        if (
+            window.scrollY >= sectionTop &&
+            window.scrollY < sectionTop + sectionHeight
+        ) {
+            current = section.id;
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+});
